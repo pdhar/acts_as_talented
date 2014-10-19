4d32d0ca229114fda1b36414b0b6080796c1b673
pramodh <satyapramodh@gmail.com>
2014-10-19 15:11:52 +0530

    ui fix

--- app/assets/javascripts/acts_as_talented/angularjs/user/UserSearchController.js
+++ app/assets/javascripts/acts_as_talented/angularjs/user/UserSearchController.js
@@ -103,6 +103,45 @@ ActsAsTalentedModule.controller("UserSearchController", function ($scope, UserSe
         "experience": "1 yr",
         "status": "Student",
         "photo": "http://t0.gstatic.com/images?q=tbn:ANd9GcR8iEXTneF6nW_3ZgQkgpL4eFUoXDsltOclt0BMyM6lA65VXfTv"
+      },
+      {
+        "name": "Batman",
+        "course": "Computers",
+        "location": "Bangalore",
+        "graduation_year": "2014",
+        "skills": "Web, vigilante, technology",
+        "about": "I am batman I am batman I am batman I am batman I am batman I am batman I am batman I am batman I am batman ",
+        "interests": "rails, ruby",
+        "age": "24",
+        "experience": "3 yr",
+        "status": "Alumni",
+        "photo": "http://media.edge-online.com/wp-content/uploads/edgeonline/2013/08/Batman-Arkham-Origins-610x343.jpg"
+      },
+      {
+        "name": "Superman",
+        "course": "Electronics",
+        "location": "Bangalore",
+        "graduation_year": "2015",
+        "skills": "Web, vigilante, strength",
+        "about": "I am superman I am superman I am superman I am superman I am superman I am superman I am superman I am superman I am superman ",
+        "interests": "java, python",
+        "age": "24",
+        "experience": "2 yr",
+        "status": "Working",
+        "photo": "http://static.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5285267ee4b048ecbebd1ce9/1384457859598/big-batman-vs-superman-movie-rumors-possible-spoilers.jpg"
+      },
+      {
+        "name": "Flash",
+        "course": "Biotechnology",
+        "location": "Bangalore",
+        "graduation_year": "2016",
+        "skills": "Web, vigilante, speed",
+        "about": "I am flash I am flash I am flash I am flash I am flash I am flash I am flash I am flash ",
+        "interests": "rails, python",
+        "age": "24",
+        "experience": "1 yr",
+        "status": "Student",
+        "photo": "http://t0.gstatic.com/images?q=tbn:ANd9GcR8iEXTneF6nW_3ZgQkgpL4eFUoXDsltOclt0BMyM6lA65VXfTv"
       }]
         
     } else {
